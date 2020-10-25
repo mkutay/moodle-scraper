990ae395d560104db47b4550254a3bb5900c466f
Mehmet Kutay Bozkurt <MKutayBozkurt@github.com>
2020-10-25 11:53:11 +0300

    Created sendMail.py

---
+++ sendMail.py
@@ -0,0 +1,27 @@
+import smtplib, ssl
+
+def send_mail(subject, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    recever_mail = '' # Kendi mailinizi koyunca o maile gonderiyor
+    sender_mail = 'moodle.bot.ari@gmail.com'
+    sender_password = 'M00dleB0t123'
+
+    server.login(sender_mail, sender_password)
+
+    msg = f"Subject: {subject}\n\n{message}"
+
+    server.sendmail(
+        sender_mail,
+        recever_mail,
+        msg.encode("utf8")
+    )
+
+    print(msg)
+
+    print("Mail has been sent")
+
+    server.quit()
