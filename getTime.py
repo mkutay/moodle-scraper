80d90fb3586dd253559d4d91f1bf1e7f015d0ece
Mehmet Kutay Bozkurt <MKutayBozkurt@github.com>
2020-10-28 22:03:44 +0300

    Created getTime.py

---
+++ getTime.py
@@ -0,0 +1,29 @@
+import urllib.parse
+import urllib.request
+import http.cookiejar
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context # Sertifika sorununu cozen sihirli satir
+
+def remTime(link, name):
+    request = urllib.request.Request(link)
+    response = urllib.request.urlopen(request)
+    contents = response.read()
+
+    f = open('.files/remTime.html', 'wb')
+    f.write(contents)
+    f.close
+
+    remTime = ""
+
+    f = open('.files/remTime.html', 'r')
+    lines = f.readlines()
+    for i in range(len(lines)):
+        s = 'Kalan süre'
+        if s in lines[i]:
+            for j in range(37, len(lines[i + 1])):
+                if lines[i + 1][j] == '<':
+                    break
+                remTime += lines[i + 1][j]
+    f.close
+    return remTime
