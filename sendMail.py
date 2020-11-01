dc994ed2d8c5427003bca90e35d415f72a9d6b49
Mehmet Kutay Bozkurt <MKutayBozkurt@github.com>
2020-11-01 13:03:42 +0300

    Created new mail

--- sendMail.py
+++ sendMail.py
@@ -1,14 +1,14 @@
 import smtplib, ssl
 
-def send_mail(subject, message, _recever_mail):
+def send_mail(subject, message, _recever_mail, _sender_mail, _sender_pass):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
 
     recever_mail = _recever_mail # Kendi mailinizi koyunca o maile gonderiyor
-    sender_mail = ''
-    sender_password = ''
+    sender_mail = _sender_mail
+    sender_password = _sender_pass
 
     server.login(sender_mail, sender_password)
 
