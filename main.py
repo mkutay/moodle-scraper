41dd52f046e07bf66d6e7a4ae2229a4f92ce5d20
Mehmet Kutay Bozkurt <47640467+MKutay@users.noreply.github.com>
2020-11-04 13:09:38 +0300

    Changed txt file to json file

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ args = sys.argv
 if os.path.isdir(".files/") == False:
     os.system("mkdir .files")
 
-if os.path.isfile(".moodle_bot_info.txt") == False or (len(args) > 1 and args[1] == '--login'):
+if os.path.isfile(".moodle_bot_info.json") == False or (len(args) > 1 and args[1] == '--login'):
     username = input('Kullanici adin nedir? ')
     password = input('Sifren nedir? ')
     recever_mail = input('Mailin nedir? ')
     sender_mail = input("Botun maili ne? ")
     sender_pass = input("Botun sifresi ne? ")
     info = {'username': username, 'password': password, 'recever_mail': recever_mail, 'sender_mail': sender_mail, 'sender_pass': sender_pass}
-    with open('.moodle_bot_info.txt', 'w') as outfile:
+    with open('.moodle_bot_info.json', 'w') as outfile:
         json.dump(info, outfile)
 
 info = {}
 
-with open('.moodle_bot_info.txt') as json_file:
+with open('.moodle_bot_info.json') as json_file:
     info = json.load(json_file)
 
 if login.scraper_login(info['username'], info['password']) == -1: # Yazdigimiz kodu cagriyoruz giris yapmasi icin
