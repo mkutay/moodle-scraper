704d7a430f4482ce4e4b90a51785ce69cb73c412
Mehmet Kutay Bozkurt <MKutayBozkurt@github.com>
2020-11-09 12:01:53 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -10,55 +10,29 @@ import sendMail
 import json
 import sys
 import getTime
+import sendHomeworkMail
 
 args = sys.argv
 
 if os.path.isdir(".files/") == False:
     os.system("mkdir .files")
 
-if os.path.isfile(".moodle_bot_info.txt") == False or (len(args) > 1 and args[1] == '--login'):
+if os.path.isfile(".moodle_bot_info.json") == False or (len(args) > 1 and args[1] == '--login'):
     username = input('Kullanici adin nedir? ')
     password = input('Sifren nedir? ')
     recever_mail = input('Mailin nedir? ')
     sender_mail = input("Botun maili ne? ")
     sender_pass = input("Botun sifresi ne? ")
     info = {'username': username, 'password': password, 'recever_mail': recever_mail, 'sender_mail': sender_mail, 'sender_pass': sender_pass}
-    with open('.moodle_bot_info.txt', 'w') as outfile:
+    with open('.moodle_bot_info.json', 'w') as outfile:
         json.dump(info, outfile)
 
 info = {}
 
-with open('.moodle_bot_info.txt') as json_file:
+with open('.moodle_bot_info.json') as json_file:
     info = json.load(json_file)
 
 if login.scraper_login(info['username'], info['password']) == -1: # Yazdigimiz kodu cagriyoruz giris yapmasi icin
     exit()
 
-tmp = findCourses.findCourses() # Ders ismini ve linkini aliyor
-names = tmp[0]
-links = tmp[1]
-
-if len(names) == 0 or len(links) == 0:
-    print("Odevin yok!! Ya da bir hata var.")
-    exit()
-
-homeworks = []
-
-for i in range(len(names)):
-    messages = isHomework.isHomework(links[i], names[i]) # Her ders icin odev var mi diye kontrol ediyor
-
-    for j in range(len(messages)):
-        remaining_time = ""
-        if 'assign' in messages[j][2]:
-            remaining_time = getTime.remTime(messages[j][2], messages[j][1])
-        else:
-            remaining_time = "Sure yok"
-        homeworks.append(messages[j][0] + " odevin var! Odevin adi: " + messages[j][1] + ". Odevin icin kalan sure: " + remaining_time + ". Odevin linki: " + messages[j][2] + ".")
-
-main_message = ""
-
-for i in range(len(homeworks)):
-    main_message += homeworks[i]
-    main_message += '\n';
-
-sendMail.send_mail("Odevlerin var!", main_message, info['recever_mail'], info['sender_mail'], info['sender_pass'])
+sendHomeworkMail.sendHomework(info)
